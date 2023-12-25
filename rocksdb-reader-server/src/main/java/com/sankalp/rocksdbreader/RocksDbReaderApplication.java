22c266ccc6d6dab085399f5d6f07098c08c02c40
Sankalp Jain <sankalpjain99@gmail.com>
2023-12-25 15:27:43 +0530

    Initial Commit

---
+++ rocksdb-reader-server/src/main/java/com/sankalp/rocksdbreader/RocksDbReaderApplication.java
@@ -0,0 +1,13 @@
+package com.sankalp.rocksdbreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocksDbReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocksDbReaderApplication.class, args);
+	}
+
+}
---
+++ rocksdb-reader-server/src/test/java/com/sankalp/rocksdbreader/RocksDbReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankalp.rocksdbreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocksDbReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
